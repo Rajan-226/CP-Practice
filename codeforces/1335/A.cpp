a9c70470271c742970c37271aea2214eac3b7bc0
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-04-14 13:54:00 +0530

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/76667984

---
+++ codeforces/1335/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   #ifndef ONLINE_JUDGE
+      freopen("/ATOM/input.txt", "r", stdin);
+      freopen("/ATOM/output.txt", "w", stdout);
+   #endif
+   int t;
+   cin>>t;
+   while(t--){
+      int n;
+      cin>>n;
+      cout<<(n-1)/2<<"\n";
+   }
+}
