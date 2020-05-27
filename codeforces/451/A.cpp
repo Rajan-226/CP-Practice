2395877ca0dbe85951f0fca0dbc03560f51037de
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-05-27 19:59:00 +0530

    Add solution for problem `A - Game With Sticks` Link: https://codeforces.com/contest/451/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/451/submission/81628437

---
+++ codeforces/451/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define nl cout<<"\n"
+#define dbg(x) cout<<x<<" ";
+#define UM unordered_map<int,int>
+#define ULL unordered_map<ll,int>
+void fun(){
+	int n,m;
+	cin>>n>>m;
+	if(min(n,m)%2==0)
+		cout<<"Malvika\n";
+	else
+		cout<<"Akshat\n";
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("/ATOM/input.txt", "r", stdin);
+		freopen("/ATOM/output.txt", "w", stdout);
+	#endif
+	// int t;
+	// cin>>t;
+	// while(t--){
+		fun();
+	// }
+}
