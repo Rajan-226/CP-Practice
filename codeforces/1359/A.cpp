b0ea8fc476b73812bed458cdca1be46148925093
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-05-29 15:32:00 +0530

    Add solution for problem `A - Berland Poker` Link: https://codeforces.com/contest/1359/problem/A Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1359/submission/81872042

---
+++ codeforces/1359/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define nl cout<<"\n"
+#define dbg(x) cout<<x<<" ";
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define UM unordered_map<int,int>
+#define ULL unordered_map<ll,int>
+void fun(){
+    int n,m,k;
+    cin>>n>>m>>k;
+    n=n/k;
+    if(m<=n){
+        cout<<m<<"\n";
+        return ;
+    }
+    m-=n;
+    k--;
+    cout<<n-ceil((double)m/k)<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
