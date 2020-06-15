c2649a113179d0a293abec28e8d0f6091a36d8a3
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-06-15 15:01:00 +0530

    Add solution for problem `B - Little Girl and Game` Link: https://codeforces.com/contest/276/problem/B Tags: games, greedy, *1300 Ref: https://codeforces.com/contest/276/submission/83827296

---
+++ codeforces/276/B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define dbg(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+int a[26]={0};
+void fun(){
+	string s;	cin>>s;
+	bool ok=1;
+	fi(0,s.size())
+		a[s[i]-'a']+=1;
+	fi(0,27)
+		if(a[i]%2)	ok=0;
+	if(ok||s.size()%2)
+		cout<<"First";
+	else
+		cout<<"Second";
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("/ATOM/input.txt", "r", stdin);
+		freopen("/ATOM/output.txt", "w", stdout);
+	#endif
+	int t=1;
+	// cin>>t;
+	while(t--){
+		fun();
+	}
+}
