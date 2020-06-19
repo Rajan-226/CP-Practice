02e5ab11f7ceeb263937b72f5ce2bb9104f45c26
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-06-19 18:11:00 +0530

    Add solution for problem `A - C+=` Link: https://codeforces.com/contest/1368/problem/A Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1368/submission/84325860

---
+++ codeforces/1368/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define pb push_back
+#define UM unordered_map<int,int>
+void fun(){
+	ll a[2],n,ans=0;
+	cin>>a[0]>>a[1]>>n;
+	sort(a,a+2);
+	while(1){
+		if(a[0]>n||a[1]>n){
+			cout<<ans<<"\n";
+			return ;
+		}
+		ans++;
+		a[0]+=a[1];
+		sort(a,a+2);
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("/ATOM/input.txt", "r", stdin);
+		freopen("/ATOM/output.txt", "w", stdout);
+	#endif
+	int t=1;
+	cin>>t;
+	while(t--){
+		fun();
+	}
+}
