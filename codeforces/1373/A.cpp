4d5eb0b5b92e1c66aeb75ace88610d3493c82175
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-06-26 11:54:00 +0530

    Add solution for problem `A - Donut Shops` Link: https://codeforces.com/contest/1373/problem/A Tags: greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1373/submission/85095699

---
+++ codeforces/1373/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define pb push_back
+#define UM unordered_map<int,int>
+void fun(){
+	ll a,b,c;
+	cin>>a>>b>>c;
+	ll ans1,ans2;
+	if(a>=c){
+		cout<<"-1 "<<b<<"\n";
+		return ;
+	}
+	else if(a*b<=c){
+		cout<<"1 -1\n";
+	}
+	else{
+		cout<<"1 "<<b<<"\n";
+	}
+	
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("/ATOM/input.txt", "r", stdin);
+		freopen("/ATOM/output.txt", "w", stdout);
+	#endif
+	int t=1;
+	cin>>t;
+	while(t--){
+		fun();
+	}
+}
