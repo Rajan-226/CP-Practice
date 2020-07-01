91ef58a77b0a7c44f61bf0c02ac56f831714304e
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-07-01 19:43:00 +0530

    Add solution for problem `B - Magical Calendar` Link: https://codeforces.com/contest/1371/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1371/submission/85653644

---
+++ codeforces/1371/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+void fun(){
+	ll n,r;
+	cin>>n>>r;
+	if(r==1){
+		cout<<"1\n";
+		return ;
+	}
+	if(n>r){
+		cout<<r*(r+1)/2<<"\n";
+	}
+	else{
+		cout<<n*(n-1)/2+1<<"\n";
+	}
+
+
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+		freopen("/ATOM/input.txt", "r", stdin);
+		freopen("/ATOM/output.txt", "w", stdout);
+	#endif
+	int t=1;
+	cin>>t;
+	while(t--){
+		fun();
+	}
+}
