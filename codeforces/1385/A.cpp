d8e8305bc472afce358d694950ec2c3ac04d6520
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-07-17 20:11:00 +0530

    Add solution for problem `A - Three Pairwise Maximums` Link: https://codeforces.com/contest/1385/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1385/submission/87081740

---
+++ codeforces/1385/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+void yn(bool check){
+	if(check)	cout<<"YES\n";
+	else		cout<<"NO\n";
+}
+void fun(){
+	int a[3];
+	cin>>a[0]>>a[1]>>a[2];
+	sort(a,a+3);
+	if(a[2]!=a[1]){
+		cout<<"NO\n";
+	}
+	else{
+		cout<<"YES\n";
+		cout<<a[2]<<" "<<a[0]<<" "<<1<<"\n";
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	 #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+	int t=1;
+	cin>>t;
+	while(t--){
+		fun();
+	}
+}
