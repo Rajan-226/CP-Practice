4f63df8d1e148e5e3d4395bfcbeed203b946b2f7
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-07-25 18:53:00 +0530

    Add solution for problem `A - Common Prefixes` Link: https://codeforces.com/contest/1384/problem/A Tags: constructive algorithms, greedy, strings, *1200 Ref: https://codeforces.com/contest/1384/submission/87989423

---
+++ codeforces/1384/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+void yn(bool check){
+    if(check)	cout<<"YES\n";
+    else		cout<<"NO\n";
+}
+void fun(){
+    int n;
+    cin>>n;
+    string ans;
+    fi(0,200)
+        ans+='a';
+    cout<<ans<<"\n";
+    int temp=0;
+    fi(0,n){
+        int k;
+        cin>>k;
+        if(ans[k]=='z')
+            ans[k]='b';
+        else
+            ans[k]+=1;
+        cout<<ans<<"\n";
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
