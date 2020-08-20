29e3ae0a3f6d3dccece3e961088ff588edb32f50
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-08-20 13:03:00 +0530

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/90442352

---
+++ codeforces/977/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+#define mxsize (int)1e6
+void fun(){
+    ll n,k;
+    cin>>n>>k;
+    while(k--){
+        if(n%10)    n--;
+        else    n/=10;
+    }
+    cout<<n<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
