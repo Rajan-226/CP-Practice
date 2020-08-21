f3494f372882e72ad48e3e3e6018a37d929fa761
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-08-21 13:42:00 +0530

    Add solution for problem `C - Fadi and LCM` Link: https://codeforces.com/contest/1285/problem/C Tags: brute force, math, number theory, *1400 Ref: https://codeforces.com/contest/1285/submission/90513791

---
+++ codeforces/1285/C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+#define mxsize (int)1e6
+void fun(){
+    ll x;
+    cin>>x;
+    ll ansa=1,ansb=x;
+    for(ll i=2;i*i<=x;i++){
+        if(x%i==0){
+            if(__gcd(i,x/i)==1){
+                ansa=i;
+                ansb=x/i;
+            }
+        }
+    }
+    cout<<ansa<<" "<<ansb<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
