bae3fa61b6dde26ede11598d5c39509bd45f5a29
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-09-04 20:07:00 +0530

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/91802146

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+#define mxsize (int)1e6
+void fun(){
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    int ans=abs(k-n);
+    ans=ceil(1.0*ans/10);
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
