50ddcbd61078f071288b4ad53a28064385ae9c11
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-09-09 15:52:00 +0530

    Add solution for problem `A - Playing with Paper` Link: https://codeforces.com/contest/527/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/527/submission/92341159

---
+++ codeforces/527/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define UM unordered_map<int,int>
+#define MP map<int,int>
+#define mxsize (int)1e6
+ll fastexp(ll a,ll n){
+    ll nod=(ll)1e18;
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    ll a,b;
+    cin>>a>>b;
+    ll ans=0;
+    while(a!=b&&a!=0&&b!=0){
+        ans+=a/b;
+        a-=b*(a/b);
+        if(a<b) swap(a,b);
+    }
+    if(ans==0)  ans++;
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
