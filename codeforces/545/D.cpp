5f2efd54e9f721a0bc1b4ff271d57bdb8535fa5b
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-09-19 18:32:00 +0530

    Add solution for problem `D - Queue` Link: https://codeforces.com/contest/545/problem/D Tags: greedy, implementation, sortings, *1300 Ref: https://codeforces.com/contest/545/submission/93193372

---
+++ codeforces/545/D.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define mxsize (int)1e6
+ll fastexp(ll a,ll n,ll nod){
+    if(nod==0)	nod=LLONG_MAX;
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n;
+    cin>>n;
+    ll a[n];
+    fi(0,n) cin>>a[i];
+    sort(a,a+n);
+    int ans=0;
+    ll time=0;
+    fi(0,n){
+        if(time<=a[i]){
+            time+=a[i];
+            ans++;
+        }
+    }
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
