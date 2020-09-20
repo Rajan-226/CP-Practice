5564a190801e07c34585e1878a85a837f9a24bef
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-09-20 12:39:00 +0530

    Add solution for problem `C - Killjoy` Link: https://codeforces.com/contest/1419/problem/C Tags: greedy, implementation, math, *1500 Ref: https://codeforces.com/contest/1419/submission/93310544

---
+++ codeforces/1419/C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define mxsize (int)1e6
+ll fastexp(ll a,ll n,ll nod){
+    if(nod==0)	nod=LLONG_MAX;
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n;
+    cin>>n;
+    int x;
+    cin>>x;
+    int a[n];
+    int d=0;
+    bool ok=0;
+    fi(0,n){
+        cin>>a[i];
+        d+=x-a[i];
+        if(a[i]==x){
+            ok=1;
+        }
+    }
+    sort(a,a+n);
+    if(a[0]==a[n-1]&&a[0]==x){
+        cout<<"0\n";
+        return ;
+    }
+    if(ok||d==0){
+        cout<<"1\n";
+    }
+    else{
+        cout<<"2\n";
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
