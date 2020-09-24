6ad5879ca2d369cb412befa56b766fcc3d7aa7d6
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-09-24 11:22:00 +0530

    Add solution for problem `A - GukiZ and Contest` Link: https://codeforces.com/contest/551/problem/A Tags: brute force, implementation, sortings, *800 Ref: https://codeforces.com/contest/551/submission/93612129

---
+++ codeforces/551/A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define mxsize (int)1e6
+ll fastexp(ll a,ll n,ll nod){
+    if(nod==0)	nod=LLONG_MAX;
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n;
+    cin>>n;
+    int a[n];
+    fi(0,n) cin>>a[i];
+    int rank=1;
+    int ans[n];
+    for(int i=2000;i>=1;i--){
+        int c=0;
+        fj(0,n){
+            if(a[j]==i){
+                c++;
+                ans[j]=rank;
+            }
+        }
+        rank+=c;
+    }
+    fi(0,n) db(ans[i]);
+    
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
