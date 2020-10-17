06d5b58f2ace36b5cfb684ade9dafd4a48b51db0
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-10-17 18:54:00 +0530

    Add solution for problem `C - ABBB` Link: https://codeforces.com/contest/1428/problem/C Tags: brute force, data structures, greedy, strings, *1100 Ref: https://codeforces.com/contest/1428/submission/95753684

---
+++ codeforces/1428/C.cpp
@@ -0,0 +1,64 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define mod 1000000007
+    #define pb push_back
+    #define nl cout<<"\n"
+    #define db(x) cout<<x<<" "
+    #define fi(a,b) for(int i=a;i<b;i++)
+    #define fj(a,b) for(int j=a;j<b;j++)
+    #define fk(a,b) for(int k=a;k<b;k++)
+    #define F first
+    #define S second
+    #define mxsize (int)1e6+5
+    ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+        ll ans=1;
+        while(n){
+            if(n&1)
+                ans=(ans*a)%nod;
+            a=(a*a)%nod;
+            n>>=1;
+        }
+        return ans;
+    }
+    void fun(){
+        string s;
+        cin>>s;
+        int n=s.size();
+        int ans=0;
+        int preb=0;
+        int a=0;
+        int i=0;
+        while(i<n){
+            if(a==0){
+                if(s[i]=='A')   a++;
+                else       preb++;
+            }
+            else{
+                if(s[i]=='B')   a--;
+                else    a++;
+            }
+            i++;
+        }
+        ans+=a;
+        ans+=preb%2;
+        cout<<ans<<"\n";
+        
+
+    }
+    int main(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        cout.precision(20);
+        cout<<fixed;
+        #ifndef ONLINE_JUDGE
+            freopen("/ATOM/input.txt", "r", stdin);
+            freopen("/ATOM/output.txt", "w", stdout);
+        #endif
+        int t=1;
+        cin>>t;
+        while(t--){
+            fun();
+        }
+    }
