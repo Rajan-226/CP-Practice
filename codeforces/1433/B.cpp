072ab384cc6feee6daf31a522f77026ae81fc48d
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-10-20 20:10:00 +0530

    Add solution for problem `B - Yet Another Bookshelf` Link: https://codeforces.com/contest/1433/problem/B Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1433/submission/96084976

---
+++ codeforces/1433/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n;
+    cin>>n;
+    int a[n];
+    fi(0,n) cin>>a[i];
+    int i=0,j=n-1;
+    int ans=0;
+    while(i<n&&a[i]==0){
+        i++;
+    }
+    while(j>=0&&a[j]==0){
+        j--;
+    }
+    while(i<j){
+        if(a[i]==0) ans++;
+        i++;
+    }
+    cout<<ans<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
