5d5a579fdb4d3b5c0b64a474dbecf01673d6ae49
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-10-20 13:55:00 +0530

    Add solution for problem `B - Modulo Sum` Link: https://codeforces.com/contest/577/problem/B Tags: combinatorics, data structures, dp, two pointers, *1900 Ref: https://codeforces.com/contest/577/submission/96048666

---
+++ codeforces/577/B.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+void fun(){
+    int n,m;
+    cin>>n>>m;
+    if(n>m){
+        cout<<"YES\n";
+        return ;
+    }
+    int a[n];
+    fi(0,n){
+        cin>>a[i];
+        a[i]%=m;
+    }
+    vector<bool> dp(m,0);
+    for(int i=0;i<n;i++){
+        vector<bool> v=dp;
+        for(int j=0;j<m;j++){
+            if(dp[j])
+                v[(j+a[i])%m]=1;
+        }
+        dp=v;
+        dp[a[i]]=1;
+    }
+    if(dp[0])   cout<<"YES\n";
+    else        cout<<"NO\n";
+
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
