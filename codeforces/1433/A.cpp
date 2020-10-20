ec2db8926651f996205d86d13c073a0e8e0959ee
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-10-20 20:21:00 +0530

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/96098698

---
+++ codeforces/1433/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+#define mxsize (int)1e6+5
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+vector<int> v;
+void fun(){
+    ll n;
+    cin>>n;
+    
+    int ans=0,dig=n%10;
+    fi(1,10){
+        if(dig==i){
+            fj(1,5){
+                ans+=j*(i-1);
+            }
+            fj(1,(int)log10(n)+2)
+                ans+=j;
+            
+            cout<<ans<<"\n";
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
