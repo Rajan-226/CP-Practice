fc938dae69eb987d9a28862658ff19793e9842af
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-10-28 18:57:00 +0530

    Add solution for problem `A - Asphalting Roads` Link: https://codeforces.com/contest/583/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/583/submission/96998623

---
+++ codeforces/583/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+
+#define vvl vector<vector<ll>>
+#define vl vector<ll>
+#define pl pair<ll,ll>
+#define ppl pair<ll,pair<ll,ll>>
+#define mxsize (int)1e6+5
+
+void fun(){
+    int n;
+    cin>>n;
+    vector<int> ans;
+    vl vis(n+4,0);
+    vl visy(n+4,0);
+    fi(1,n*n+1){
+        int x,y;
+        cin>>x>>y;
+        if(vis[x]==0&&visy[y]==0){
+            vis[x]=1;
+            visy[y]=1;
+            ans.push_back(i);
+        }
+    }
+    for(auto i:ans) db(i);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
