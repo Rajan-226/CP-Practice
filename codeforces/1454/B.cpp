3ab168d0f9cb453fde51cd62061b250e72199a71
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-11-24 20:13:00 +0530

    Add solution for problem `B - Unique Bid Auction` Link: https://codeforces.com/contest/1454/problem/B Tags: implementation, *800 Ref: https://codeforces.com/contest/1454/submission/99420219

---
+++ codeforces/1454/B.cpp
@@ -0,0 +1,73 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+
+#define vvl vector<vector<ll>>
+#define vl vector<ll>
+#define pl pair<ll,ll>
+#define ppl pair<ll,pair<ll,ll>>
+#define mxsize (int)1e6+5
+
+void fun(){
+    int n;
+    cin>>n;
+    map<int,int> mp;
+    int a[n+1];
+    fi(1,n+1){
+        cin>>a[i];
+        mp[a[i]]++;
+    }
+    int num=-1;
+    for(auto i:mp){
+        if(i.S==1){
+            num=i.F;
+            break;
+        }
+    }
+    fi(1,n+1){
+        if(a[i]==num){
+            cout<<i<<"\n";
+            return ;
+        }
+    }
+    cout<<"-1\n";
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
