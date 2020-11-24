82e07d9e491a5f395cfd22ce91d29a4f862509f9
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-11-24 11:23:00 +0530

    Add solution for problem `B - Anton and Lines` Link: https://codeforces.com/contest/593/problem/B Tags: geometry, sortings, *1600 Ref: https://codeforces.com/contest/593/submission/99366252

---
+++ codeforces/593/B.cpp
@@ -0,0 +1,66 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+
+#define vvl vector<vector<ll>>
+#define vl vector<ll>
+#define pl pair<ll,ll>
+#define ppl pair<ll,pair<ll,ll>>
+#define mxsize (int)1e6+5
+
+void fun(){
+    ll n,x1,x2;
+    cin>>n>>x1>>x2;
+    vector<pair<ll,ll>> v;
+    fi(0,n){
+        ll k,b;
+        cin>>k>>b;
+        v.push_back({k*x1+b,k*x2+b});
+    }
+    sort(v.begin(),v.end());
+    fi(0,n-1){
+        if(v[i+1].S<v[i].S){
+            cout<<"YES\n";
+            return ;
+        }
+    }
+    cout<<"NO\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
