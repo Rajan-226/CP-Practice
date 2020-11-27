11fa8131fff253db798a341e344c50902834240c
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-11-27 16:21:00 +0530

    Add solution for problem `C - Swap Adjacent Elements` Link: https://codeforces.com/contest/920/problem/C Tags: dfs and similar, greedy, math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/920/submission/99707388

---
+++ codeforces/920/C.cpp
@@ -0,0 +1,80 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+
+#define vvl vector<vector<ll>>
+#define vl vector<ll>
+#define pl pair<ll,ll>
+#define ppl pair<ll,pair<ll,ll>>
+#define mxsize (int)1e6+5
+
+void fun(){
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    fi(0,n){
+        cin>>a[i];
+        b[i]=a[i];
+    }
+    sort(b,b+n);
+    string s;
+    cin>>s;
+    fi(0,n){
+        if(a[i]==b[i])  continue;
+        if(a[i]<b[i]){
+            if(s[i-1]=='0'){
+                cout<<"NO\n";
+                return ;
+            }
+            continue;
+        }
+        int j=i;    
+        while(i<n-1&&b[i]!=a[j]&&s[i]=='1'){
+            i++;
+        }
+        if(b[i]!=a[j]){
+            cout<<"NO\n";
+            return ;
+        }
+        i--;
+    }
+    cout<<"YES\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
