7336eaa4d374c6a14d32d2b709560325284fda10
Rajan <rajan0184.cse19@chitkara.edu.in>
2020-12-15 20:07:00 +0530

    Add solution for problem `A - Favorite Sequence` Link: https://codeforces.com/contest/1462/problem/A Tags: implementation, two pointers, *800 Ref: https://codeforces.com/contest/1462/submission/101259498

---
+++ codeforces/1462/A.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)1e5
+
+void fun(){
+    int n;
+    cin>>n;
+    int a[n];
+    fi(0,n) cin>>a[i];
+    int i=0,j=n-1;
+    bool ok=1;
+    while(i<=j){
+        if(ok){
+            cout<<a[i++]<<" ";
+            ok=0;
+        }else{
+            cout<<a[j--]<<" ";
+            ok=1;
+        }
+    }nl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
