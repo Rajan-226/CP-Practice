d7ce3e341749fa2d38b280b8424a276376abbb8a
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-02 14:25:00 +0530

    Add solution for problem `C - Day at the Beach` Link: https://codeforces.com/contest/599/problem/C Tags: sortings, *1600 Ref: https://codeforces.com/contest/599/submission/103014570

---
+++ codeforces/599/C.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+
+void fun(){     //ab aaya hai, aise language kaise smjh skta koi
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    fi(0,n){
+        cin>>a[i];
+        b[i]=a[i];
+    }
+    sort(b,b+n);
+    map<int,int> mp;
+    int ans=0;
+    int cnt=0;
+    fi(0,n){
+        if(!mp[b[i]])   cnt++;
+        mp[b[i]]++;
+        mp[a[i]]--;
+        if(!mp[a[i]])   cnt--;
+        if(cnt==0){
+            ans++;
+        }
+    }
+    cout<<ans<<"\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
