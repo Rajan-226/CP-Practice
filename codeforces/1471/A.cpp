c33ce3633e9c2a69a0af7c06c5edbb7ded4bd000
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-05 20:08:00 +0530

    Add solution for problem `A - Strange Partition` Link: https://codeforces.com/contest/1471/problem/A Tags: greedy, math, number theory, *900 Ref: https://codeforces.com/contest/1471/submission/103392221

---
+++ codeforces/1471/A.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+
+void fun(){
+    ll n,m;
+    cin>>n>>m;
+    ll a[n];
+    ll ans=0;
+    ll sum=0;
+    fi(0,n){
+        cin>>a[i];
+        sum+=a[i];
+        ans+=ceil(1.0*a[i]/m);
+    }
+    sum=ceil(1.0*sum/m);
+    cout<<sum<<" "<<ans<<"\n";
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
