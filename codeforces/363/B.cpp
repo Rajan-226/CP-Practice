4a4c15ead25ce44de937646b18aa6490b246a114
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-08 19:48:00 +0530

    Add solution for problem `B - Fence` Link: https://codeforces.com/contest/363/problem/B Tags: brute force, dp, *1100 Ref: https://codeforces.com/contest/363/submission/103724697

--- codeforces/363/B.cpp
+++ codeforces/363/B.cpp
@@ -1,28 +1,62 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+
+void fun(){
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    fi(0,n) cin>>a[i];
+    int ms=0,ind=1,j=0;
+    fi(0,k) ms+=a[k];
+    int sum=ms;
+    fi(k,n){
+        ms-=a[j++];
+        ms+=a[i];
+        if(ms<sum){
+            sum=ms;
+            ind=j+1;
+        }
+    }
+    cout<<ind<<"\n";
+}
+
 int main(){
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
-   cout.tie(NULL);
-   #ifndef ONLINE_JUDGE
-      freopen("/ATOM/input.txt", "r", stdin);
-      freopen("/ATOM/output.txt", "w", stdout);
-   #endif
-   int n,k,ans=0,ind=1;
-   scanf("%d %d",&n,&k);
-   int a[n];
-   for(int i=0;i<n;i++)
-      scanf("%d",&a[i]);
-   for(int i=0;i<k;i++)
-      ans+=a[i];
-   int sum=ans;
-   for(int i=k;i<n;i++){
-      sum+=a[i]-a[i-k];
-      if(sum<ans){
-         ans=sum;
-         ind=i-k+2;
-      }
-   }
-   cout<<ind;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
 }
