7aa5b65c6854e477923ca301f7ae792e19d282aa
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-09 22:34:00 +0530

    Add solution for problem `C - Registration System` Link: https://codeforces.com/contest/4/problem/C Tags: data structures, hashing, implementation, *1300 Ref: https://codeforces.com/contest/4/submission/103901009

---
+++ codeforces/4/C.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+
+void fun(){  //Timepass
+    map<string,int> mp;
+    int n;
+    cin>>n;
+    fi(0,n){
+        string s;
+        cin>>s;
+        if(!mp[s]){
+            cout<<"OK\n";
+        }else{
+            cout<<s<<mp[s]<<"\n";
+        }
+        mp[s]++;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
