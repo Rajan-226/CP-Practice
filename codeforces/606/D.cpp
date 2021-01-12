24acdd3d0f2d675c3ac39c2ce51f7bad279ac64f
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-12 14:49:00 +0530

    Add solution for problem `D - Lazy Student` Link: https://codeforces.com/contest/606/problem/D Tags: graphs, *1700 Ref: https://codeforces.com/contest/606/submission/104098180

---
+++ codeforces/606/D.cpp
@@ -0,0 +1,92 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+struct node{
+    int w;
+    int mst;
+    int index;
+};
+bool comp(node &A,node &B){
+    if(A.w!=B.w)    return A.w<B.w;
+    return A.mst>B.mst;
+}
+void fun(){
+    int n,m;
+    cin>>n>>m;
+    vector<node> v(m);
+    vector<pair<int,int>> ans(m);
+    fi(0,m){
+        cin>>v[i].w>>v[i].mst;
+        v[i].index=i;
+    }
+    sort(v.begin(),v.end(),comp);
+    int curnode=1;
+    int maker=3,makel=1;
+    fi(0,m){
+        int a,b;
+        if(!v[i].mst){
+            if(maker>curnode){
+                cout<<"-1\n";
+                return ;
+            }
+            a=maker;
+            b=makel;
+            makel--;
+            if(makel==0){
+                maker++;
+                makel=maker-2;
+            }
+        }
+        else{
+            a=curnode;
+            b=curnode+1;
+            curnode++;
+        }
+        int ind=v[i].index;
+        ans[ind].F=a;
+        ans[ind].S=b;
+    }
+    fi(0,m){
+        cout<<ans[i].F<<" "<<ans[i].S<<"\n";
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
