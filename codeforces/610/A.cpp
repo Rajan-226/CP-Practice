2d310c9ed8ad6491a3e63ba9496cb4283cb44387
Rajan <rajan0184.cse19@chitkara.edu.in>
2021-01-13 14:20:00 +0530

    Add solution for problem `A - Pasha and Stick` Link: https://codeforces.com/contest/610/problem/A Tags: combinatorics, math, *1000 Ref: https://codeforces.com/contest/610/submission/104177328

---
+++ codeforces/610/A.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define pi 3.141592653589
+#define pb push_back
+#define nl cout<<"\n"
+#define db(x) cout<<x<<" "
+#define fi(a,b) for(int i=a;i<b;i++)
+#define fj(a,b) for(int j=a;j<b;j++)
+#define fk(a,b) for(int k=a;k<b;k++)
+#define F first
+#define S second
+ll fastexp(ll a,ll n,ll nod=LLONG_MAX){
+    ll ans=1;
+    while(n){
+        if(n&1)
+            ans=(ans*a)%nod;
+        a=(a*a)%nod;
+        n>>=1;
+    }
+    return ans;
+}
+#define mxsize (int)3e5
+
+void fun(){
+    int n;
+    cin>>n;
+    int ans=0;
+    if(n%2==0){
+        ans=n/4;
+        if(n%4==0)  ans--;
+    }
+    cout<<ans<<"\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout.precision(20);
+    cout<<fixed;
+    #ifndef ONLINE_JUDGE
+        freopen("/ATOM/input.txt", "r", stdin);
+        freopen("/ATOM/output.txt", "w", stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    while(t--){
+        fun();
+    }
+}
